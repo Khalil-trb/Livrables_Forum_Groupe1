5ef630a782ebb7006460e1f23d1d29a3ab15288d
Khalil-trb <khalil.trabelsi@ynov.com>
2026-05-28 10:08:15 +0200

    changement couleur texte

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'forum_db',
   waitForConnections: true,
   connectionLimit: 10,
